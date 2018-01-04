112076055c721b24553f60faefe5069f281882e2
Alexander Hansson <alexander.hansson@bulldozer.se>
2018-01-04 17:52:09 +0100

    felhantering :fire:

--- cli.js
+++ cli.js
@@ -3,12 +3,17 @@
 
 const meow = require('meow')
 const prisjakt = require('./index.js')
+const chalk = require('chalk')
 
 const cli = meow(`
 	Usage
 	  $ prisjakt <sökord>
 	Example
-	  $ prisjakt 'hifiman 400'
+	  $ prisjakt hifiman 400
 `)
 
-prisjakt(cli.input.join(' '))
+if(cli.input.length > 0) {
+  prisjakt(cli.input.join(' '))
+} else {
+  console.log(chalk.bold.red('Fel: ') + 'Du behöver ange minst ett ord att söka efter. Om du behöver hjälp: ' + chalk.green('prisjakt --help'))
+}
